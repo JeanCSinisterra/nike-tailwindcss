d89fffc315425758c664e57551c62e3ef90c7611
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-10-08 21:09:04 -0500

    SuperQuality section just finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,11 +7,13 @@ const Button = ({label, iconURL}) => {
     rounded-full text-white
     border-coral-red'>
         {label}
+
+        {iconURL && 
         <img 
         src={iconURL} 
         alt="arrow right icon" 
         className="ml-2 rounded-full w-5 h-5"
-        />
+        />}
     </button>
   )
 }
